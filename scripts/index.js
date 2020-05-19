f8d803e1564f2f64aaca1c14935dbd5fef0d5ceb
Naruto <singhvedprakash075@gmail.com>
2020-05-19 06:33:25 +0530

    easter egg

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,10 @@ var navItem = document.querySelector("nav ul");
 burger.addEventListener("click", function(){
     navItem.classList.toggle("view");
 })
+
+
+// No touching
+var myPic = document.querySelector(".pic img");
+myPic.addEventListener("click", function () {
+    alert("No Touching!!!");
+})

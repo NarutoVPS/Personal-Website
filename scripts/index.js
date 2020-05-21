6d00bbac7c6726253ace4f836467b54a24e0366a
Naruto <singhvedprakash075@gmail.com>
2020-05-21 13:58:42 +0530

    updated index.js

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,21 @@ var navItem = document.querySelector("nav ul");
 burger.addEventListener("click", function(){
     navItem.classList.toggle("view");
 })
+
+
+// No touching
+var myPic = document.querySelector(".pic img");
+myPic.addEventListener("click", function () {
+    alert("No Touching!!!");
+})
+
+// Navbar Scroll
+$(function () {
+	$(window).on("scroll", function () {
+		if ($(window).scrollTop() > 100) {
+			$("nav").addClass("navAppear");
+		} else {
+			$("nav").removeClass("navAppear");
+		}
+	});
+});
